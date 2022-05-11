30fc730cb78a40cf5bf48a63602194aa197be66f
NanasoDev <mariano.ceballos@gmail.com>
2022-05-11 15:44:38 -0300

    Primer Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
-@Component({
+@Component( {
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  styleUrls: [ './app.component.css' ]
+} )
+
 export class AppComponent {
-  title = 'angular-pdf-viewer';
+
+  // DECLARO LA VARIABLE CON VALOR POR DEFECTO
+  pdfSrc = '';
+
+  constructor() {
+
+    function getQueryVariable() {
+      var query = window.location.search.substring( 1 );
+      const final = query.substring( 4 );
+      return final;
+    }
+
+    // CARGO LA URL DEL PDF EN LA VARIABLE QUE LEE LA LIBRERIA
+    this.pdfSrc = getQueryVariable();
+
+    // IMPRIMO LA URL  DEL PDF
+    console.log( 'PDFSRC ' + this.pdfSrc );
+
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
+
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PdfViewerModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
