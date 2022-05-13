96a3ca9bcc41eb5a76f28eeb6dd59f4460168fa4
Natalia Moyano <moyanonatalia7@gmail.com>
2022-05-13 17:01:38 -0300

    Icon + Title + Zoom

--- src/app/visor-pdf/visor-pdf.component.ts
+++ src/app/visor-pdf/visor-pdf.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class VisorPdfComponent implements OnInit {
 
   // Declare the variable empty by default
-  pdfSrc = '';
+  pdfSrc = '../assets/example.pdf';
 
   constructor() {}
 
